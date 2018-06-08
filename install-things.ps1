b07b66d62231b7e00ec1efca7a509711c2fcf90e
ctro <clint@ctro.net>
2018-06-08 08:38:31 -0600

    add wordweb

--- install-things.ps1
+++ install-things.ps1
@@ -2,7 +2,7 @@
 $toInstall = "7zip", "clipx", "docker-for-windows", "electrum", "fiddler", `
     "googleearth", "handbrake", "lightshot", "paint.net", "ruby", "seer", `
     "slack", "spotify", "steam", "tixati", "vim", "visualstudiocode", `
-    "poshgit", "windirstat"
+    "poshgit", "windirstat", "wordweb-free"
 
 choco upgrade -y ($toInstall -join ";")
 
